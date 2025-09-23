18881a1df00b1b594825edeabf8d0d926ccf065c
abdurrahmantaherr <abdurrahmantaherrr@gmail.com>
2025-09-23 10:42:06 +0500

    docs: add myutils man page and install target, finalize Feature-5

--- src/main.c
+++ src/main.c
@@ -5,23 +5,32 @@
 int main() {
     char str1[100] = "Hello";
     char str2[100] = "World";
-    mystrcpy(str1, "Hi");    
-    
+
+    printf("Initial str1: %s\n", str1);
+    printf("Initial str2: %s\n", str2);
+
+    mystrcpy(str1, "Hi");
+    printf("After mystrcpy, str1: %s\n", str1);
+
     mystrncpy(str2, "Everyone", 4);
-    
+    str2[4] = '\0';   // ensure null termination
+    printf("After mystrncpy, str2: %s\n", str2);
+
     mystrcat(str1, str2);
-    printf("Combined string: %s\n", str1);
-    printf("Length: %d\n", mystrlen(str1));
+    printf("After mystrcat, str1: %s\n", str1);
+
+    printf("mystrlen(str1): %d\n", mystrlen(str1));
 
     FILE* file = fopen("test.txt", "r");
     if (!file) {
-    return 1;
+        printf("Could not open test.txt\n");
+        return 1;
     }
     int lines, words, chars;
     wordCount(file, &lines, &words, &chars);
     fclose(file);
 
-    printf("Lines: %d, Words: %d, Chars: %d\n", lines, words, chars);
+    printf("File stats -> Lines: %d, Words: %d, Chars: %d\n", lines, words, chars);
 
     return 0;
 }
